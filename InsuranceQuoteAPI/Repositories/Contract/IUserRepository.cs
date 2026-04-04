ae311bf4982db4ebbcb145db9ae6e1a0877c5b59
vocuongthinh2@gmail.com <vocuongthinh2@gmail.com>
2026-04-04 22:03:13 +0700

    Push code

---
+++ InsuranceQuoteAPI/Repositories/Contract/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using InsuranceQuoteAPI.Models;
+
+namespace InsuranceQuoteAPI.Repositories.Contract
+{
+    public interface IUserRepository
+    {
+        Task<List<User>> GetUsersAsync();
+        Task<User?> GetUserAsync(String name);
+        Task<User> AddUserAsync(User user);
+        Task<User> UpdatedUserAsync(User user);
+        Task<User> DeleteUserAsync(int id);
+    }
+}
---
+++ InsuranceQuoteAPI/Repositories/Implementations/UserRepository.cs
@@ -0,0 +1,57 @@
+﻿using InsuranceQuoteAPI.Data;
+using InsuranceQuoteAPI.Models;
+using InsuranceQuoteAPI.Repositories.Contract;
+using Microsoft.EntityFrameworkCore;
+
+namespace InsuranceQuoteAPI.Repositories.Implementations
+{
+    public class UserRepository : IUserRepository
+    {
+        private InsuranceQuoteContext _context;
+
+        public UserRepository(InsuranceQuoteContext context)
+        {
+            _context = context;
+        }
+        public async Task<User> AddUserAsync(User user)
+        {
+            await _context.AddAsync(user);
+            await _context.SaveChangesAsync();
+            return user;
+        }
+
+        public async Task<User> DeleteUserAsync(int id)
+        {
+            var FindUserDelete = await _context.Users.FirstOrDefaultAsync(u => u.Id == id);
+            if(FindUserDelete != null)
+            {
+                _context.Users.Remove(FindUserDelete);
+                await _context.SaveChangesAsync();
+            }
+            return FindUserDelete;
+
+        }
+
+        public async Task<User?> GetUserAsync(string name)
+        {
+            return await _context.Users.FirstOrDefaultAsync(u => u.UserName == name);
+        }
+
+        public async Task<List<User>> GetUsersAsync()
+        {
+            var ListUsers = await _context.Users.ToListAsync();
+            return ListUsers;
+        }
+
+        public async Task<User> UpdatedUserAsync(User user)
+        {
+             var AccountUpdate = _context.Users.FirstOrDefault(u => u.Id == user.Id);
+            if (AccountUpdate != null) { 
+                AccountUpdate.FullName = user.FullName;
+                _context.Users.Update(AccountUpdate);
+                await _context.SaveChangesAsync();
+            }
+            return AccountUpdate;
+        }
+    }
+}
